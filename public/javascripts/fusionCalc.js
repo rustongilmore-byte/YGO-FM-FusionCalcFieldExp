9f9666b2c4b98e97b4b4ea9d0971230c115c9f6e
rustongilmore-byte <rustongilmore@gmail.com>
2026-07-18 20:50:14 -0500

    Update fusionCalc.js

--- public/javascripts/fusionCalc.js
+++ public/javascripts/fusionCalc.js
@@ -1,159 +1,299 @@
-var outputLeft = document.getElementById("outputarealeft");
-var outputRight = document.getElementById("outputarearight");
+const outputLeft = document.getElementById("outputarealeft");
+const outputRight = document.getElementById("outputarearight");
 
-// Initialize Awesomplete
-var _awesompleteOpts = {
-    list: card_db()
-        .get()
-        .map((c) => c.Name), // List is all the cards in the DB
-    autoFirst: true, // The first item in the list is selected
-    filter: Awesomplete.FILTER_STARTSWITH, // Case insensitive from start of word
+const INPUT_GROUPS = {
+  hand: {
+    count: 5,
+    label: "Hand",
+  },
+  field: {
+    count: 5,
+    label: "Field",
+  },
 };
-var handCompletions = {};
-for (i = 1; i <= 5; i++) {
-    var hand = document.getElementById("hand" + i);
-    handCompletions["hand" + i] = new Awesomplete(hand, _awesompleteOpts);
+
+// Initialize Awesomplete.
+const awesompleteOptions = {
+  list: card_db()
+    .get()
+    .map((card) => card.Name),
+  autoFirst: true,
+  filter: Awesomplete.FILTER_STARTSWITH,
+};
+
+const completions = {};
+
+Object.keys(INPUT_GROUPS).forEach((groupName) => {
+  const group = INPUT_GROUPS[groupName];
+
+  for (let slot = 1; slot <= group.count; slot += 1) {
+    const inputId = groupName + slot;
+    const input = document.getElementById(inputId);
+    completions[inputId] = new Awesomplete(input, awesompleteOptions);
+  }
+});
+
+function getCardByName(cardName) {
+  return card_db({ Name: { isnocase: cardName } }).first();
 }
 
-// Creates a div for each fusion
-function fusesToHTML(fuselist) {
-    return fuselist
-        .map(function (fusion) {
-            var res =
-                "<div class='result-div'>Input: " +
-                fusion.card1.Name +
-                "<br>Input: " +
-                fusion.card2.Name;
-            if (fusion.result) {
-                // Equips and Results don't have a result field
-                res += "<br>Result: " + fusion.result.Name;
-                if (isMonster(fusion.result)) {
-                    res += " " + formatStats(fusion.result.Attack, fusion.result.Defense);
-                } else {
-                    res += " [" + cardTypes[fusion.result.Type] + "]";
-                }
-            }
-            return res + "<br><br></div>";
-        })
-        .join("\n");
-}
-
-function getCardByName(cardname) {
-    return card_db({ Name: { isnocase: cardname } }).first();
-}
-
-// Returns the card with a given ID
 function getCardById(id) {
-    var card = card_db({ Id: id }).first();
-    if (!card) {
-        return null;
-    }
-    return card;
+  return card_db({ Id: id }).first() || null;
 }
 
 function formatStats(attack, defense) {
-    return "(" + attack + "/" + defense + ")";
+  return "(" + attack + "/" + defense + ")";
 }
 
-// Returns true if the given card is a monster, false if it is magic, ritual,
-// trap or equip
+// Monster types occupy the first 20 entries in this project's card database.
 function isMonster(card) {
-    return card.Type < 20;
+  return card.Type < 20;
 }
 
-function checkCard(cardname, infoname) {
-    var info = $("#" + infoname);
-    var card = getCardByName(cardname);
-    if (!card) {
-        info.html("Invalid card name");
-    } else if (isMonster(card)) {
-        info.html(formatStats(card.Attack, card.Defense) + " [" + cardTypes[card.Type] + "]");
-    } else {
-        info.html("[" + cardTypes[card.Type] + "]");
+function checkCard(cardName, infoId) {
+  const info = $("#" + infoId);
+  const card = getCardByName(cardName);
+
+  if (!card) {
+    info.html("Invalid card name");
+  } else if (isMonster(card)) {
+    info.html(formatStats(card.Attack, card.Defense) + " [" + cardTypes[card.Type] + "]");
+  } else {
+    info.html("[" + cardTypes[card.Type] + "]");
+  }
+}
+
+function getCardsFromGroup(groupName) {
+  const cards = [];
+  const group = INPUT_GROUPS[groupName];
+
+  for (let slot = 1; slot <= group.count; slot += 1) {
+    const name = $("#" + groupName + slot).val();
+    const card = getCardByName(name);
+
+    if (card) {
+      cards.push({
+        card,
+        zone: group.label,
+        slot,
+      });
     }
+  }
+
+  return cards;
 }
 
-// Checks if the given card is in the list of fusions
-// Assumes the given card is an Object with an "Id" field
-// TODO: Generalize to take Object, Name (string) or Id (int)
-function hasFusion(fusionList, card) {
-    return fusionList.some((c) => c.Id === card.Id);
+function findFusion(card1, card2) {
+  const firstList = fusionsList[card1.Id] || [];
+  const direct = firstList.find((fusion) => fusion.card === card2.Id);
+
+  if (direct) {
+    return getCardById(direct.result);
+  }
+
+  // The database should normally contain both directions, but checking the
+  // reverse direction makes field support safer if an entry is one-sided.
+  const secondList = fusionsList[card2.Id] || [];
+  const reverse = secondList.find((fusion) => fusion.card === card1.Id);
+
+  return reverse ? getCardById(reverse.result) : null;
+}
+
+function canEquip(card1, card2) {
+  const firstList = equipsList[card1.Id] || [];
+  const secondList = equipsList[card2.Id] || [];
+
+  return firstList.includes(card2.Id) || secondList.includes(card1.Id);
+}
+
+function compareEntries(entry1, entry2, source) {
+  const fusionResult = findFusion(entry1.card, entry2.card);
+
+  return {
+    fusion: fusionResult
+      ? {
+          entry1,
+          entry2,
+          result: fusionResult,
+          source,
+        }
+      : null,
+    equip: canEquip(entry1.card, entry2.card)
+      ? {
+          entry1,
+          entry2,
+          source,
+        }
+      : null,
+  };
+}
+
+function compareWithinGroup(entries, source) {
+  const fusions = [];
+  const equips = [];
+
+  for (let first = 0; first < entries.length - 1; first += 1) {
+    for (let second = first + 1; second < entries.length; second += 1) {
+      const comparison = compareEntries(entries[first], entries[second], source);
+
+      if (comparison.fusion) {
+        fusions.push(comparison.fusion);
+      }
+
+      if (comparison.equip) {
+        equips.push(comparison.equip);
+      }
+    }
+  }
+
+  return { fusions, equips };
+}
+
+function compareBetweenGroups(firstGroup, secondGroup, source) {
+  const fusions = [];
+  const equips = [];
+
+  firstGroup.forEach((entry1) => {
+    secondGroup.forEach((entry2) => {
+      const comparison = compareEntries(entry1, entry2, source);
+
+      if (comparison.fusion) {
+        fusions.push(comparison.fusion);
+      }
+
+      if (comparison.equip) {
+        equips.push(comparison.equip);
+      }
+    });
+  });
+
+  return { fusions, equips };
+}
+
+function entryLabel(entry) {
+  return entry.zone + " " + entry.slot;
+}
+
+function resultsToHTML(results) {
+  if (results.length === 0) {
+    return "<p class='text-center text-muted'>None found.</p>";
+  }
+
+  return results
+    .map((result) => {
+      let html =
+        "<div class='result-div'>" +
+        "<strong>" +
+        result.source +
+        "</strong>" +
+        "<br>" +
+        entryLabel(result.entry1) +
+        ": " +
+        result.entry1.card.Name +
+        "<br>" +
+        entryLabel(result.entry2) +
+        ": " +
+        result.entry2.card.Name;
+
+      if (result.result) {
+        html += "<br>Result: " + result.result.Name;
+
+        if (isMonster(result.result)) {
+          html += " " + formatStats(result.result.Attack, result.result.Defense);
+        } else {
+          html += " [" + cardTypes[result.result.Type] + "]";
+        }
+      }
+
+      return html + "</div>";
+    })
+    .join("\n");
+}
+
+function resultAttack(result) {
+  if (!result.result || !isMonster(result.result)) {
+    return 0;
+  }
+
+  return Number(result.result.Attack) || 0;
 }
 
 function findFusions() {
-    var cards = [];
-    var monsters = [];
-    var others = [];
+  const handCards = getCardsFromGroup("hand");
+  const fieldCards = getCardsFromGroup("field");
 
-    for (i = 1; i <= 5; i++) {
-        var name = $("#hand" + i).val();
-        var card = getCardByName(name);
-        if (card) {
-            cards.push(card);
-        }
-    }
+  const handResults = compareWithinGroup(handCards, "Hand + Hand");
+  const fieldResults = compareBetweenGroups(handCards, fieldCards, "Hand + Field");
 
-    var fuses = [];
-    var equips = [];
+  // Cards already sitting on the field cannot be selected together as a new
+  // play, so Field + Field combinations are intentionally not calculated.
+  const fusions = handResults.fusions.concat(fieldResults.fusions);
+  const equips = handResults.equips.concat(fieldResults.equips);
 
-    for (i = 0; i < cards.length - 1; i++) {
-        var card1 = cards[i];
-        var card1Fuses = fusionsList[card1.Id];
-        var card1Equips = equipsList[card1.Id];
-        for (j = i + 1; j < cards.length; j++) {
-            var card2 = cards[j];
-            var fusion = card1Fuses.find((f) => f.card === card2.Id);
-            if (fusion) {
-                fuses.push({ card1: card1, card2: card2, result: getCardById(fusion.result) });
-            }
-            var equip = card1Equips.find((e) => e === card2.Id);
-            if (equip) {
-                equips.push({ card1: card1, card2: card2 });
-            }
-        }
-    }
+  fusions.sort((a, b) => resultAttack(b) - resultAttack(a));
 
-    outputLeft.innerHTML = "<h2 class='center'>Fusions:</h2>";
-    outputLeft.innerHTML += fusesToHTML(fuses.sort((a, b) => b.result.Attack - a.result.Attack));
-
-    outputRight.innerHTML = "<h2 class='center'>Equips:</h2>";
-    outputRight.innerHTML += fusesToHTML(equips);
+  outputLeft.innerHTML = "<h2 class='center'>Fusions:</h2>" + resultsToHTML(fusions);
+  outputRight.innerHTML = "<h2 class='center'>Equips:</h2>" + resultsToHTML(equips);
 }
 
 function resultsClear() {
-    outputLeft.innerHTML = "";
-    outputRight.innerHTML = "";
+  outputLeft.innerHTML = "";
+  outputRight.innerHTML = "";
 }
 
-function inputsClear() {
-    for (i = 1; i <= 5; i++) {
-        $("#hand" + i).val("");
-        $("#hand" + i + "-info").html("");
+function clearInputGroup(groupName) {
+  const group = INPUT_GROUPS[groupName];
+
+  for (let slot = 1; slot <= group.count; slot += 1) {
+    $("#" + groupName + slot).val("");
+    $("#" + groupName + slot + "-info").html("");
+  }
+}
+
+function refreshResults() {
+  resultsClear();
+  findFusions();
+}
+
+function setUpInput(inputId) {
+  $("#" + inputId).on("change", function () {
+    completions[this.id].select();
+
+    if (this.value === "") {
+      $("#" + this.id + "-info").html("");
+    } else {
+      checkCard(this.value, this.id + "-info");
     }
+
+    refreshResults();
+  });
+
+  $("#" + inputId).on("awesomplete-selectcomplete", function () {
+    checkCard(this.value, this.id + "-info");
+    refreshResults();
+  });
 }
 
-// Set up event listeners for each card input
-for (i = 1; i <= 5; i++) {
-    $("#hand" + i).on("change", function () {
-        handCompletions[this.id].select(); // select the currently highlighted element
-        if (this.value === "") {
-            // If the box is cleared, remove the card info
-            $("#" + this.id + "-info").html("");
-        } else {
-            checkCard(this.value, this.id + "-info");
-        }
-        resultsClear();
-        findFusions();
-    });
+Object.keys(INPUT_GROUPS).forEach((groupName) => {
+  const group = INPUT_GROUPS[groupName];
 
-    $("#hand" + i).on("awesomplete-selectcomplete", function () {
-        checkCard(this.value, this.id + "-info");
-        resultsClear();
-        findFusions();
-    });
-}
+  for (let slot = 1; slot <= group.count; slot += 1) {
+    setUpInput(groupName + slot);
+  }
+});
+
+$("#resetHandBtn").on("click", function () {
+  clearInputGroup("hand");
+  refreshResults();
+});
+
+$("#resetFieldBtn").on("click", function () {
+  clearInputGroup("field");
+  refreshResults();
+});
 
 $("#resetBtn").on("click", function () {
-    resultsClear();
-    inputsClear();
+  clearInputGroup("hand");
+  clearInputGroup("field");
+  resultsClear();
 });
